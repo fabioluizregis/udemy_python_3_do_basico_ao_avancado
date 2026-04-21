9c90f0894f92f43d229b08ca8c052450b618e6b8
Fabio Luiz Régis <fabioluizregis@Fabios-MacBook-Air.local>
2026-04-21 19:37:28 -0300

    Aula 1 - Comentarios e DocString

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # udemy_python_3_do_basico_ao_avancado
+
+Python 3+ completo: PySide6, Django, Selenium, Regexp, Testes, TDD, POO, Design Patterns GoF, algoritmos e programação
+
+link do curso: udemy.com/course/python-3-do-zero-ao-avancado
+professor: Luiz Otávio Miranda
+carga horária: 141.5 horas
---
+++ exercicios/aula1.py
@@ -0,0 +1,11 @@
+""""
+    Esse espaço é conhecido como DocString
+    Normalmente usado para documentar funções ou
+    projetos.
+"""
+
+# Comentário de código!
+# Isso é um comentário acima do código.
+print(123) # Isso é um comentário na frente do código.
+# Isso é um comentário abaixo do código.
+

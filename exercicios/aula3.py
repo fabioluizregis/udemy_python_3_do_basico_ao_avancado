2f717d8b05a321683b4a658675aedd8ee3359971
Fabio Luiz Régis <fabioluizregis@Fabios-MacBook-Air.local>
2026-04-22 08:10:11 -0300

    Aula 3 - strings

---
+++ exercicios/aula3.py
@@ -0,0 +1,17 @@
+""""
+Python = Linguagem de Programação
+Tipo de Tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+# Aspas Simples
+print('Fabio Luiz')
+print('Fabio "Luiz"')
+# Aspas Duplas
+print("Fabio Luiz")
+print("Fabio 'Luiz'")
+# Escape
+print("Fabio \"Luiz\"")
+# r -> raw string -> string crua
+print(r"Fabio \"Luiz\"")

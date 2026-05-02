7f2f4c4a67af675d26274f643e129141631f9484
Fabio <fabioluizregis@gmail.com>
2026-05-02 09:04:42 -0300

    Aula 8 - Exercício com variáveis e tipos de dados

---
+++ exercicios/aula8.py
@@ -0,0 +1,16 @@
+# Execício práticocom variáveis e tipos de dados
+
+nome = 'Dino'
+sobrenome = 'Sauro'
+idade = 300
+ano_nascimento = 1726
+maior_de_idade = idade >= 18
+altura_metros = 3.14
+
+
+print('Nome:', nome)
+print('Sobrenome', sobrenome)
+print('Idade', idade)
+print('Ano de nasciento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)

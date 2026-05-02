7aff5d112129da6ee8d9f8d20968b9ecc355fff3
Fabio <fabioluizregis@gmail.com>
2026-05-02 08:03:09 -0300

    Aula 6 - Coerção de tipos

---
+++ exercicios/aula6.py
@@ -0,0 +1,11 @@
+# Conversão de tipos, coerção, type convertion,
+# typecasting, coercion. É o ato de converter 
+# um tipo em outros tipos imutáveise primitivos.
+# str, int, float, bool
+
+print(int('1'), type(int('1')))
+print(int('1') + 1)
+print(float('1') + 1)
+print(bool(''))        # String vazia = False
+print(bool('XPTO'))     # String com qualquer coisa dentro = True
+print(str(11) + 'b')

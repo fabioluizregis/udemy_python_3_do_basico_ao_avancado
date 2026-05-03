7cc098b73cd067eda6482ae5b5baed39e61efa32
Fabio <fabioluizregis@gmail.com>
2026-05-03 08:18:40 -0300

    Aula 12 - Exercício - Cálculo do IMC

---
+++ aula12.py
@@ -0,0 +1,9 @@
+# Cálculo do IMC + Ellipsis
+
+nome = 'Fabio Luiz'
+altura = 1.81
+peso = 96
+
+imc = peso / (altura * altura)
+
+print (nome, 'tem', altura, 'de altura, pesa', peso, 'kilos e seu IMC é', imc)

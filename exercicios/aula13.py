d7321a148728158b446f1ec5601b5a3c5f4c1f44
Fabio <fabioluizregis@gmail.com>
2026-05-03 08:55:02 -0300

    Aula 13 - Infrodução às fstrings

---
+++ exercicios/aula13.py
@@ -0,0 +1,9 @@
+# Introdução às fstrings
+
+nome = 'Fabio Luiz'
+altura = 1.81
+peso = 96
+
+imc = peso / (altura * altura)
+
+print(f"{nome} tem {altura:.2f} de altura, pesa {peso} quilos e seu IMC é {imc}")

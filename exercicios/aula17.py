d0ae6912086ca48b9c2dc90372baa570ddfe2236
Fabio <fabioluizregis@gmail.com>
2026-05-24 16:46:03 -0300

    Aula 17 - Revisão -  Blocos condicionais if, elif e else

---
+++ exercicios/aula17.py
@@ -0,0 +1,24 @@
+# if / elif      / else
+# se / se não se / se não
+
+condicao1 = False
+condicao2 = True
+condicao3 = False
+condicao4 = True
+
+if condicao1:
+    print('Este é o código para condição 1')
+elif condicao2:
+    print('Este é o código para condição 2')
+elif condicao3:
+    print('Este é o código para condição 3')
+elif condicao4:
+    print('Este é o código para condição 4')
+else:
+    print('Este é o else do primeiro if')
+
+
+if 10 == 10:
+    print('Outro if!')
+
+print('Fora do if')

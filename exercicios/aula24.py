baf0aa7b306b582259fcb4ed759c4a25dd84bc05
Fabio <fabioluizregis@gmail.com>
2026-07-22 07:35:36 -0300

    Aula 24 - Operadores IN e NOT IN

---
+++ exercicios/aula24.py
@@ -0,0 +1,14 @@
+# Operadores in e not in
+# Strings são iteráveis.
+#  0 1 2 3 4 
+#  F A B I O 
+# -5-4-3-2-1
+
+nome = 'Fabio'
+print(nome[2])
+print(nome[-3])
+
+print('i' in nome)
+print('z' not in nome)
+
+print('Fab' in nome)

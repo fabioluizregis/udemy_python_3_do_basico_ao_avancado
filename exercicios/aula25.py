8f24552fff4cb863b30aba4c34cbce5232b5983f
Fabio <fabioluizregis@gmail.com>
2026-07-22 07:49:50 -0300

    Aula 25 - Interpolação de string com %

---
+++ exercicios/aula25.py
@@ -0,0 +1,15 @@
+"""
+Interpolação básica de strings
+
+s - string
+d e i - int
+f - float
+x e X - Hexadecimal (ABCDEF0123456789)
+"""
+
+nome = 'Fabio'
+preco = 1000.658887984
+variavel = '%s, o preço é R$%.2f' % (nome, preco)
+print(variavel)
+
+print('O hexadecimal de %d é %04x' %(1500,1500))
